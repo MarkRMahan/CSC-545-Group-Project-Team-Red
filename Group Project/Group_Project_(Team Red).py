a7e1a8e49660100b069b12cea2e8e775cfade298
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2018-03-01 13:13:38 -0600

    Add files via upload
    Starting the project!

---
+++ Group Project/Group_Project_(Team Red).py
@@ -0,0 +1,15 @@
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Group Project: Team Red
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Authors:
+    Mark R. Mahan
+    Joshua Yamdogo
+    Samuel Trenter
+    Shinya Honda
+    Humberto Colin
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This project is meant to run a platforming game
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+print("hello world")

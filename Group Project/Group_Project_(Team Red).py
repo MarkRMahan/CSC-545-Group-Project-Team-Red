b5d7e5d7d86e41652d4c7f3c4235939f9549da67
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2018-03-01 13:28:11 -0600

    Add files via upload
    Adding more of the basics

--- Group Project/Group_Project_(Team Red).py
+++ Group Project/Group_Project_(Team Red).py
@@ -12,4 +12,28 @@ Authors:
 This project is meant to run a platforming game
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
-print("hello world")
+
+from tkinter import *
+
+class Platformer(Frame):
+    
+    def __init__(self):
+        Frame.__init__(self)
+        self.master.title("Platformer") #Frame's title
+        self.grid()
+        
+        self.height = 600
+        self.width = 1000
+        self.canvas = Canvas(self, height = 600, width = 1000, bg = "white")
+        self.canvas.grid(row = 1, column = 0)
+        
+        self.Game() #Runs the "game"
+        
+    def Game(self):
+        print("hello world")
+        
+        
+def main():
+    Platformer().mainloop()
+    
+main()
